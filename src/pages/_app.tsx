e8d20a453761896abed788aaab7b717783a5ab6b
Anish George <anish.george@fabricgroup.com.au>
2024-01-30 14:23:29 +1100

    Get sorted with initial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { CssVarsProvider } from "../stuff/themeProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <CssVarsProvider>
+      <Component {...pageProps} />
+    </CssVarsProvider>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import getInitColorSchemeScript from "@mui/system/cssVars/getInitColorSchemeScript";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        {getInitColorSchemeScript(/** options here will be same as in <CssVarsProvider> */)}
         <Main />
         <NextScript />
       </body>
