93a88513b95982455e27017a39776a26c25dc993
Anish George <anish.george@fabricgroup.com.au>
2024-02-02 11:53:16 +1100

    Add inferred variables

--- src/stuff/Button.tsx
+++ src/stuff/Button.tsx
@@ -1,7 +1,7 @@
 import { styled } from '@mui/system';
 
 const Button = styled('button')(({ theme }) => ({
-  backgroundColor: theme.vars.palette.primary.default,
+  backgroundColor: theme.vars.palette.info.primary,
   border: `1px solid ${theme.vars.palette.primary.dark}`,
   color: theme.vars.palette.text.default,
 }));
--- src/stuff/extendTheme.ts
+++ src/stuff/extendTheme.ts
@@ -1,56 +1,67 @@
 import {
   unstable_createGetCssVar as systemCreateGetCssVar,
   unstable_prepareCssVars as prepareCssVars,
-  createTheme
-} from '@mui/system';
+  createTheme,
+  Theme as MuiTheme,
+} from "@mui/system";
 
-declare module '@mui/system' {
+declare module "@mui/system" {
   interface Theme {
-    vars: {
-      palette: Record<string, any>
-    }
+    vars: Omit<
+      MuiTheme,
+      "typography" | "mixins" | "breakpoints" | "direction" | "transitions"
+    >;
   }
 }
 
 const lightColorScheme = {
   palette: {
-    mode: 'light',
+    mode: "light",
     primary: {
-      default: '#3990FF',
-      dark: '#02367D',
+      default: "#3990FF",
+      dark: "#02367D",
     },
     text: {
-      default: '#111111',
+      default: "#111111",
     },
     // ... other colors
   },
 };
 
+function genContextual(obj: any) {
+  return {
+    palette: {
+      info: {
+        primary: obj.palette.primary.default,
+      },
+    },
+  };
+}
+
 const darkColorScheme = {
   palette: {
-    mode: 'dark',
+    mode: "dark",
     primary: {
-      default: '#265D97',
-      dark: '#132F4C',
-      main: '#5090D3',
+      default: "#265D97",
+      dark: "#132F4C",
+      main: "#5090D3",
     },
     text: {
-      default: '#ffffff',
+      default: "#ffffff",
     },
     // ... other colors
   },
 };
 
 // Don't know what the use of this is yet
-const createGetCssVar = (cssVarPrefix = 'my-app') =>
+const createGetCssVar = (cssVarPrefix = "my-app") =>
   systemCreateGetCssVar(cssVarPrefix);
 
-function extendTheme({ cssVarPrefix = 'my-app', baseTheme = {} } = {}) {
+function extendTheme({ cssVarPrefix = "my-app", baseTheme = {} } = {}) {
   const theme: any = {
     ...baseTheme,
     colorSchemes: {
       light: lightColorScheme,
-      dark: darkColorScheme,
     },
   };
 
@@ -58,21 +69,47 @@ function extendTheme({ cssVarPrefix = 'my-app', baseTheme = {} } = {}) {
     { colorSchemes: theme.colorSchemes },
     {
       prefix: cssVarPrefix,
-    },
+    }
   );
-  theme.vars = themeVars;
-  theme.generateCssVars = generateCssVars;
-  theme.palette = {
-    ...theme.colorSchemes.light.palette,
-    colorScheme: 'light',
+
+  const { vars: contextualVars, generateCssVars: conGen } = prepareCssVars(
+    { colorSchemes: { light: genContextual(themeVars) } },
+    { prefix: cssVarPrefix }
+  );
+
+  theme.vars = {
+    palette: {
+      ...themeVars.palette,
+      ...contextualVars.palette,
+    },
   };
 
+  theme.generateCssVars = (colorScheme: any) => {
+    const base = generateCssVars(colorScheme);
+    const extended = conGen(colorScheme);
+
+    const newLocal = {
+      css: {
+        ...base.css,
+        ...extended.css,
+      },
+      vars: { // We basically need to do a deep merge
+        palette: {
+          ...base.vars.palette,
+          ...extended.vars.palette,
+        },
+      },
+    };
+    return newLocal;
+  };
+
+  theme.palette = null;
   return theme;
 }
 
 const baseTheme = createTheme({
   // any stuff apart from colors,
-  spacing: [0, 2, 5, 10, 20]
+  spacing: [0, 2, 5, 10, 20],
 });
 
 const myCustomDefaultTheme = extendTheme({ baseTheme });
